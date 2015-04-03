f8fa3ceaffeacd36dc9ca4c5991f4790b2c44f9e
Loetkolben <SirLoetkolben@gmail.com>
2015-04-03 22:08:48 +0200

    Initial commit
    Fork from https://github.com/jimeh/docker-znc
    - Enabled ZNC Python3 support
    - Enabled SSL in znc.conf.default
    - Dockerfile: now FROM ubuntu:14.04

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM        ubuntu:14.04
+MAINTAINER  Loetkolben "SirLoetkolben@gmail.com"
+
+# We use a bootstrap script to avoid having temporary cache files and build
+# dependencies being committed and included into the docker image.
+ADD         bootstrap.sh /tmp/
+RUN         chmod +x /tmp/bootstrap.sh && /tmp/bootstrap.sh
+
+# UID 9001 because of reasons
+RUN         useradd -u 9001 znc
+ADD         start-znc /usr/local/bin/
+ADD         znc.conf.default /src/
+RUN         chmod 644 /src/znc.conf.default
+
+EXPOSE      6667
+ENTRYPOINT  ["/usr/local/bin/start-znc"]
+CMD         [""]
---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+set -e
+
+# Config
+ZNC_VERSION="1.6.0"
+
+
+# Ensure package list is up to date.
+apt-get update
+
+# Install runtime dependencies.
+apt-get install -y sudo
+
+# Install build dependencies.
+apt-get install -y wget build-essential libssl-dev libperl-dev pkg-config python3-dev libicu-dev
+
+
+# Prepare building
+mkdir -p /src
+
+
+# Download, compile and install ZNC.
+cd /src
+wget "http://znc.in/releases/archive/znc-${ZNC_VERSION}.tar.gz"
+tar -zxf "znc-${ZNC_VERSION}.tar.gz"
+cd "znc-${ZNC_VERSION}"
+./configure --enable-python && make && make install
+
+
+# Clean up
+apt-get remove -y wget
+apt-get autoremove -y
+apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
